22072c26d2cc1e3e075b0e6cd5245bdec66d62d4
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-03-26 05:55:39 +0530

    Add initial files

---
+++ src/main/java/com/ashish/authorityservice/AuthorityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.authorityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorityServiceApplication.class, args);
+	}
+
+}
