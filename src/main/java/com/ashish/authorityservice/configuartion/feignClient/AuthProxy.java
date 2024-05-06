d814845280899e3d36cb71135e897d023e77e7fc
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-05-06 16:34:15 +0530

    modify config for hosting

--- src/main/java/com/ashish/authorityservice/configuartion/feignClient/AuthProxy.java
+++ src/main/java/com/ashish/authorityservice/configuartion/feignClient/AuthProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "auth-service",url = "http://3.109.184.195:8080")
+@FeignClient(name = "auth-service",url = "http://3.111.235.72:8080")
 public interface AuthProxy {
     @PostMapping("/api/v1/auth/updatePayment")
     public void updatePayment(@RequestBody PaymentData paymentData);
